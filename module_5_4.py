e18f0a5ecd4bd2cf41c86a05f86a3eee2b29194c
ulkeralizade <ulker.100504@gmail.com>
2024-11-21 16:36:55 +0400

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,73 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        house_name = args[0]
+        cls.houses_history.append(house_name)
+        return super().__new__(cls)
+
+    def __init__(self, name, floor):
+        self.name = name
+        self.number_of_floors = floor
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int) and value > 0:
+            self.number_of_floors += value
+        else:
+            print("Добавляемое число должно быть пложительным числом!")
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
